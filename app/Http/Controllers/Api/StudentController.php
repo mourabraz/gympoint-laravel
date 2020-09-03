45c976971e6d634debb16aab2dfbe77ee58a2212
mourabraz <cmourabraz@gmail.com>
2020-09-03 16:51:42 +0100

    update Student Controller

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -17,13 +18,13 @@ class StudentController extends Controller
         //
     }
 
-    public function update()
+    public function update(Student $student)
     {
-        //
+        return $student;
     }
 
-    public function destroy()
+    public function destroy(Student $student)
     {
-        //
+        return $student;
     }
 }
