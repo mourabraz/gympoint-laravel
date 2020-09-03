297097b512503c1ee58887e13ddd24d7b3b9d3a9
mourabraz <cmourabraz@gmail.com>
2020-09-03 12:00:09 +0100

    create controllers

---
+++ app/Http/Controllers/Api/CheckinController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CheckinController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
