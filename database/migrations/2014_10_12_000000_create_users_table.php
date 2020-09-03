86ad059d3335deee668268d285becfee2df01822
mourabraz <cmourabraz@gmail.com>
2020-09-03 09:28:46 +0100

    add field to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestampTz('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'student'])->default('student');
             $table->rememberToken();
             $table->timestampsTz();
         });
